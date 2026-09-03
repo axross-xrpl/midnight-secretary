675be509c54eb5b045b4db434bd53e4bbe551199
yahomi-dev <yahomi.dev@gmail.com>
2026-09-03 17:59:19 +0900

    Format src/i18n/request.ts with Biome

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,20 +1,20 @@
-import {hasLocale} from 'next-intl';
-import {getRequestConfig} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import * as rootParams from 'next/root-params';
+import { hasLocale } from "next-intl";
+import { getRequestConfig } from "next-intl/server";
+import { notFound } from "next/navigation";
+import * as rootParams from "next/root-params";
 
-import {routing} from './routing';
+import { routing } from "./routing";
 
 export default getRequestConfig(async () => {
-	const requested = await rootParams.locale();
+  const requested = await rootParams.locale();
 
-	if (!hasLocale(routing.locales, requested)) {
-		notFound();
-	}
-	const locale = requested;
+  if (!hasLocale(routing.locales, requested)) {
+    notFound();
+  }
+  const locale = requested;
 
-	return {
-		locale,
-		messages: (await import(`../../messages/${locale}.json`)).default
-	};
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default,
+  };
 });
